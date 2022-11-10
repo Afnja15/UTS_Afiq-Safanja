d1e24091afe5c29b6efdfe0080a27422339fc407
Afnja15 <114881264+Afnja15@users.noreply.github.com>
2022-11-10 18:24:54 +0700

    Add files via upload

---
+++ js/scrip.js
@@ -0,0 +1,11 @@
+const txtElement = ['Freelancer', 'Mahasiswa Teknologi PLN',];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+		console.log('txtElement')
+	
+})();
